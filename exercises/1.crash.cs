bb0080c3c2775d24892e771251823bfdf4a416bb
rishab25718 <chatter@stpaulsschool.org.uk>
2026-05-12 11:53:56 +0100

    Update 1.crash.cs

--- exercises/1.crash.cs
+++ exercises/1.crash.cs
@@ -1,5 +1,18 @@
 string[] names = { "Alice", "Bob", "Charlie" };
 Console.Write("Enter an index: ");
-int i = int.Parse(Console.ReadLine()!);
-Console.WriteLine(names[i]);
-// crash
+
+try
+{
+    int i = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(names[i]);
+}
+
+catch (FormatException e)
+{
+    Console.WriteLine($"That is not an integer:  {e.Message}");
+}
+
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine($"That index is too big: {e.Message}");
+}
