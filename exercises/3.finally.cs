cb359c295dd0da43d08dc3e474783bfd30701729
rishab25718 <chatter@stpaulsschool.org.uk>
2026-05-14 15:39:30 +0100

    Update 3.finally.cs

--- exercises/3.finally.cs
+++ exercises/3.finally.cs
@@ -1,2 +1,25 @@
 // exercise 3
 
+Console.WriteLine("Enter an integer:");
+var input = Console.ReadLine();
+
+try
+{
+    int num = int.Parse(input);
+}
+
+catch (FormatException e)
+{
+    Console.WriteLine($"That is not an integer: {e.Message}");
+}
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+
+else if (num % 2 == 1)
+{
+    Console.WriteLine("Odd");
+}
+
