d2cb6e3041cb4c9da3a2720781959f3a6670a118
rishab25718 <chatter@stpaulsschool.org.uk>
2026-05-15 10:37:01 +0100

    Update 6.temp.cs

--- exercises/6.temp.cs
+++ exercises/6.temp.cs
@@ -1,7 +1,37 @@
 
+class CONSTANTS
+{
+    public decimal ABSOLUTEZERO = -273.15;
+}
+
 class TemperatureException : Exception
 {
     // your constructors here
+
+    public TemperatureException();
+
+    public TemperatureException(string message)
+    {
+        base(message);
+    }
+
+    public TemperatureException(string message, Exception inner)
+    {
+        base(message, inner);
+    }
+}
+
+class AbsoluteZeroError : TemperatureException
+{
+    public decimal temperature { get; }
+
+    if (decimal temperature < CONSTANTS.ABSOLUTEZERO)
+    {
+        
+    }
 }
 
 // program below...
+
+
+decimal temperature = 0m;
