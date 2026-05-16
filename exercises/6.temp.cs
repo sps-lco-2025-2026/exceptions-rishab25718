9b1aab5e7682c49881571a28f0e1fb1149bacbc5
rishab25718 <chatter@stpaulsschool.org.uk>
2026-05-16 17:35:03 +0100

    Update 6.temp.cs

--- exercises/6.temp.cs
+++ exercises/6.temp.cs
@@ -1,37 +1,49 @@
-
-class CONSTANTS
-{
-    public decimal ABSOLUTEZERO = -273.15;
-}
+using System;
 
 class TemperatureException : Exception
 {
-    // your constructors here
-
-    public TemperatureException();
+    public TemperatureException()
+    {
+    }
 
     public TemperatureException(string message)
+        : base(message)
     {
-        base(message);
     }
 
     public TemperatureException(string message, Exception inner)
+        : base(message, inner)
     {
-        base(message, inner);
     }
 }
 
 class AbsoluteZeroError : TemperatureException
 {
     public decimal temperature { get; }
-
-    if (decimal temperature < CONSTANTS.ABSOLUTEZERO)
-    {
-        
-    }
 }
 
 // program below...
 
+Console.WriteLine("Enter temperature in Celcius: ");
 
-decimal temperature = 0m;
+try
+{
+    double TemperatureCelcius = double.Parse(Console.ReadLine()!);
+
+    if (TemperatureCelcius < -273.15)
+    {
+        throw new TemperatureException("Temperature is below absolute zero");
+    }
+
+    double TemperatureFarenheit = TemperatureCelcius * 9 / 5 + 32;
+
+    Console.WriteLine($"{TemperatureCelcius}°C = {TemperatureFarenheit}°F");
+}
+catch (TemperatureException e)
+{
+    Console.WriteLine($"Temperature error: {e.Message}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Enter a valid temperature");
+}
